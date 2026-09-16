048e039be19d091120d4d3489559bfa45bb5ad8a
Danny Akulov <akulov.danny@gmail.com>
2026-09-16 17:57:06 +0300

    feat: add ssl

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from os import getenv
 app = Flask(__name__)
 
 def get_db():
-    conn = psycopg2.connect(getenv('DATABASE_URL'), timeout=5)
+    conn = psycopg2.connect(getenv('DATABASE_URL'), connect_timeout=5)
     return conn
 
 @app.route('/')
